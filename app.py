a116dc6e9221ed4c73f24aca8374184a54650e4a
CCVE <jscheveto7@gmail.com>
2023-07-12 00:08:20 -0500

    created liked entry button

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, url_for, flash, redirect, request
 from flask_behind_proxy import FlaskBehindProxy
 import secrets
 from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
 
 key = secrets.token_hex(16)
 app = Flask(__name__)
@@ -18,20 +20,33 @@ class Liked(db.Model):
     
     def __repr__(self):
         return f"User('{self.id}', '{self.description}', '{self.url}')"
-    
+
+class SaveSearch(FlaskForm):
+    submit = SubmitField('Liked')
+
 with app.app_context():
+    # db.drop_all()
     db.create_all()
 
 @app.route("/",methods=['GET', 'POST'])
 @app.route("/index",methods=['GET', 'POST'])
 def index():
-    if request.method == 'POST':
-        if request.form.get('action1') == 'Liked':
-            print('works')
-            saved_liked(60, "hat", "cat") # do something
-            print('data')
-            print(db.session.query(15))
-    return render_template('index.html')
+    form = SaveSearch()
+    print('hi')
+    if form.validate_on_submit():
+        print('works')
+        saved_liked(24, "hat", "cat") # do something
+        print('data')
+        print(db.session.query(Liked).all())
+    # if request.method == 'POST':
+    #     if request.form['action1'] == 'Liked':
+    #         print('works')
+    #         saved_liked(21, "hat", "cat") # do something
+    #         print('data')
+    #         print(db.session.query(Liked).all())
+    #     else:
+    #         pass
+    return render_template('index.html', form = form)
 
     
 @app.route("/liked")
