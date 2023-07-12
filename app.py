fc4ee52cf71d47b361dc964215a02425767df2c8
David <daveozzy6@gmail.com>
2023-07-11 23:41:34 -0500

    parse frontend data to backend

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template, url_for, flash, redirect
+from urllib import request
+from flask import Flask, render_template, url_for, flash, redirect,request
 from flask_behind_proxy import FlaskBehindProxy
+from main_ebay import Ebay_21
+import requests
 import secrets
 
 key = secrets.token_hex(16)
@@ -9,9 +12,17 @@ app.config['SECRET_KEY'] = key
 
 
 @app.route("/")
-@app.route("/index")
+@app.route("/index", methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        search_query = request.form.get('search')
+        flash(f"Search Query: {search_query}")
+        ebay = Ebay_21(name=search_query)
+        # Use the ebay object as needed
+        #flash(f"Search Query: {search_query}")
+        return f"<p>{ebay.retrieve_data_from_database()}</p>"
+        #return redirect(url_for('liked'))  # Redirect back to the index page after form submission
+    return render_template('base.html')
     
 @app.route("/liked")
 def liked():
@@ -20,6 +31,8 @@ def liked():
 @app.route("/products")
 def products():
     return render_template('products.html')
+
+
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port = 8001)
--- main_ebay.py
+++ main_ebay.py
@@ -4,14 +4,15 @@ from ebaysdk.exception import ConnectionError
 from ebaysdk.finding import Connection
 import os
 import sqlite3
+import pandas as pd
 import pprint
 load_dotenv()
 API_KEY = os.getenv('api_key')
 
 
 class Ebay_21(object):
-    def __init__(self, API_KEY, name) -> None:
-        self.api_key = API_KEY
+    def __init__(self, name) -> None:
+        self.api_key = os.getenv('api_key')
         self.name = name
         self.db_connection = sqlite3.connect('ebay_data.db')  # Connect to the database
         self.db_cursor = self.db_connection.cursor()
@@ -60,34 +61,62 @@ class Ebay_21(object):
             return False
 
     def retrieve_data_from_database(self):
-        try:
-            self.db_cursor.execute("SELECT * FROM {}".format(self.name))
-            rows = self.db_cursor.fetchall()
+        # try:
+        #     conn = sqlite3.connect('ebay_data.db')
+        #     qry = self.db_cursor.execute("SELECT * FROM {}".format(self.name))
+        #     rows = self.db_cursor.fetchall()
+        #
+        #     # data_dict = []
+        #     # for row in rows:
+        #     #     item_id = row[0]
+        #     #     name = row[1]
+        #     #     image = row[2]
+        #     #     price = row[3]
+        #     #     link = row[4]
+        #     #     data_dict[item_id] = {
+        #     #         'name': name,
+        #     #         'image': image,
+        #     #         'price': price,
+        #     #         'link': link
+        #     #     }
+        #
+        #     #return data_dict
+        #
+        #     #self.save_products_to_database()
+        #     #conn = sqlite3.connect('zappos_data.db')
+        #     # Execute a query to retrieve data
+        #     #qry = "SELECT * FROM products;"
+        #     with conn as connection:
+        #         rows = self.db_cursor.fetchall()
+        #         zappos_db = pd.DataFrame(rows)
+        #     conn.close()
+        #     return zappos_db
+        #
+        #
+        # except Exception as e:
+        #     print("Error occurred:", e)
+        #     return None
+        self.fetch()
 
-            data_dict = {}
-            for row in rows:
-                item_id = row[0]
-                name = row[1]
-                image = row[2]
-                price = row[3]
-                link = row[4]
-                data_dict[item_id] = {
-                    'name': name,
-                    'image': image,
-                    'price': price,
-                    'link': link
-                }
+        pd.set_option('display.max_columns', None)
+        pd.set_option('display.max_rows', None)
+        pd.set_option('display.width', None)
 
-            return data_dict
-        except Exception as e:
-            print("Error occurred:", e)
-            return None
+        conn = sqlite3.connect('ebay_data.db')
+        # Execute a query to retrieve data
+        qry = "SELECT * FROM {}".format(self.name)
+        with conn as connection:
+            query_result = connection.execute(qry).fetchall()
+            zappos_db = pd.DataFrame(query_result,columns=["index", "name", "image", "price", "link"])
+            #first_ten_rows = zappos_db.head(10)
+        conn.close()
+        return zappos_db
 
     def parse(self):
         pass
 
 
 if __name__ == '__main__':
-    e = Ebay_21(API_KEY, 'iphone')
+    e = Ebay_21('iphone')
     e.fetch()
-    e.parse()
+    print(e.retrieve_data_from_database())
